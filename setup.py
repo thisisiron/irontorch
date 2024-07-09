dd84db75eb652e307f07782798c16685e29bb7b9
root <kimiron518@gmail.com>
2024-07-09 19:41:39 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="irontorch",
+    version="0.0.1",
+    description="Developer-friendly Pytorch utility library",
+    url="https://github.com/thisisiron/irontorch",
+    author="Kim Eon",
+    author_email="kimiron518@gmail.com",
+    license="MIT",
+    install_requires=[
+        "torch>=1.1",
+    ],
+    packages=find_packages(),
+)
