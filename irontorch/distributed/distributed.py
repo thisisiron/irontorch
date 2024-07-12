161cfe82ff9e7a5c4d3b16468acb84647291e62b
root <kimiron518@gmail.com>
2024-07-12 18:30:48 +0900

    add parallel func.

--- irontorch/distributed/distributed.py
+++ irontorch/distributed/distributed.py
@@ -10,3 +10,15 @@ def get_rank():
         return 0
 
     return dist.get_rank()
+
+
+def is_parallel(model):
+   """Determines whether the model is a parallel model (DP or DDP)."""
+   return isinstance(model, (nn.parallel.DataParallel, nn.parallel.DistributedDataParallel))
+
+
+def upwrap_parallel(model):
+   """Converts a parallel model (DP or DDP) to a non-parallel model."""
+   return model.module if is_parallel(model) else model
+
+
