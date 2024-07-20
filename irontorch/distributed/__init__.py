52629923e04960a10fe62edebdbd1ae4b73ccbac
root <kimiron518@gmail.com>
2024-07-21 03:37:18 +0900

    update init

--- irontorch/distributed/__init__.py
+++ irontorch/distributed/__init__.py
@@ -1,3 +1,12 @@
-from .distributed import (
+from irontorch.distributed import (
     get_rank,
+    is_primary,
+    synchronize,
+    get_world_size,
+    reduce_dict,
+    is_parallel,
+    upwrap_parallel,
+    get_data_sampler,
 )
+
+from irontorch.launch import run
