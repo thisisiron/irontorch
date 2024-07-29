815e79832eecee93dc2df12608487eaae66f0e3c
root <kimiron518@gmail.com>
2024-07-30 01:22:12 +0900

    add test code

--- irontorch/recoder/recoder.py
+++ irontorch/recoder/recoder.py
@@ -1,6 +1,50 @@
 import os
+import sys
+import logging
 
 from irontorch import distributed as dist
+
+
+def get_logger(name, save_dir, distributed_rank=None, filename="log.txt"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    # don't log results for the non-master process
+    if distributed_rank > 0:
+        return logger
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+            "%(asctime)s %(name)s %(levelname)s: %(message)s",
+            datefmt="%m/%d %H:%M:%S"
+    )
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    if save_dir:
+        fh = logging.FileHandler(os.path.join(save_dir, filename))
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+
+class Logger:
+    def __init__(self):
+        self.logger = get_logger()
+
+    def log(self, step, **kwargs):
+        panels = [f"step: {step}"]
+
+        for k, v in kwargs.items():
+            if isinstance(v, float):
+                decimal = get_decimal(v) + 2
+                v = round(v, decimal)
+                panels.append(f"{k}: {v}")
+
+            else:
+                panels.append(f"{k}: {v}")
+        self.logger.info("| ".join(panels))
 
 
 class WandB:
