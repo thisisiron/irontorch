6dfe25dc0f141dbd5b3239274aa1e48790beb833
root <kimiron518@gmail.com>
2024-08-11 19:37:10 +0900

    fix typo

--- irontorch/distributed/schema.py
+++ irontorch/distributed/schema.py
@@ -1,10 +1,10 @@
 from typing import Union, Any, Optional
 from pydantic import BaseModel, StrictBool
 
+
 class Config(BaseModel):
     class Config:
         extra = 'allow'
     launch_config: Any
     distributed: Optional[StrictBool]
-
 
